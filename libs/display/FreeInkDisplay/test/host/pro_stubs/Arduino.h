e171030aaf6bbacb82e19989164233abef48dfbe
Antoine Aflalo <197810+Belphemur@users.noreply.github.com>
2026-09-14 22:38:42 -0400

    [UX] feat: instrument UC8279 X4 AA refresh policy (#24)

--- libs/display/FreeInkDisplay/test/host/pro_stubs/Arduino.h
+++ libs/display/FreeInkDisplay/test/host/pro_stubs/Arduino.h
@@ -10,3 +10,7 @@
 inline unsigned long millis() { static unsigned long t; return ++t; }
 inline void delay(unsigned long) {}
 inline int digitalRead(int) { return 0; }
+struct SerialStub {
+  void printf(const char*, ...) {}
+};
+inline SerialStub Serial;
